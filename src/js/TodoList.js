3a13051c2ff012a1042014ed3090d6f6dc2fb506
Herman Prawiro <blackridergx@gmail.com>
2017-01-02 16:36:19 +0700

    [Todos] Add key to each row in TodoList. [General] Add watch command to webpack.

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class TodoList extends React.Component {
 
-  displayTask(task) {
-    return (<li>{task}</li>);
+  displayTask(task, key) {
+    return (<li key={key}>{task}</li>);
   }
 
   render() {
     return (
       <ul>
-        {this.props.tasks.map(this.displayTask)}
+        {this.props.tasks.map((value, key) => this.displayTask(value, key))}
       </ul>
     );
   }
